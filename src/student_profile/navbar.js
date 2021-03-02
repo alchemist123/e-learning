c22f3ca187912ee13adf98796a3190980ae68eda
Unni <69468073+hash-dotcom@users.noreply.github.com>
2021-03-02 19:42:43 +0530

    init

---
+++ src/student_profile/navbar.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import {link} from
+
+function navbar() {
+    return (
+     <>
+     <nav className="navbar"></nav>
+        <div className="navbar-container">
+            <link to="/" className="navbar-logo">
+       </link>
+        </div>
+     </>
+    )
+}
+
+export default navbar
